0a0f7690a2c79daadb207b9a239c3288c63ba2c7
Teddochi <tedochiai@gmail.com>
2022-08-01 18:06:30 -0700

    Loaded server side props into a simple component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const DateList = ({title, dates}) => {
+  return (
+    <div>
+      <p>{title} Dates</p>
+      {dates.map(function(d, idx) {
+        return (<li key={idx}>{d}</li>)
+      })}
+    </div>
+  );
+};
+
+const Home: NextPage = (data: any) => {
+  // TODO: DRY this up
+  let melee : String [] = data.data.data.Melee;
+  let golf : String [] = data.data.data.Golf;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,40 +32,15 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get really started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {/* TODO: DRY these up*/}
+        <DateList
+          title="Melee"
+          dates={melee}
+        />
+        <DateList
+          title="Golf"
+          dates={golf}
+        />
       </main>
 
       <footer className={styles.footer}>
@@ -67,6 +57,16 @@ const Home: NextPage = () => {
       </footer>
     </div>
   )
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:3000/api/dates`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
 
 export default Home
